644f629cfbed6880ad502144a53e717ad7c7bae6
z-alex <al_zhang@outlook.com>
2017-10-12 19:35:10 -0400

    Added a test for the methods set and get of the User class

---
+++ tests/Unit/UserClassTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserClassTest extends TestCase {
+
+    /**
+     * A test for the get and set methods of the user class
+     *
+     * @return void
+     */
+    public function testSetGetTest() {
+        $user = new User();
+
+        $userData = new \stdClass();
+
+        $userData->id = '1';
+        $userData->firstName = 'John';
+        $userData->lastName = 'Doe';
+        $userData->email = 'johndoe123@gmail.com';
+        $userData->phone = '123-456-7890';
+        $userData->admin = '0';
+        $userData->physicalAddress = '1234 Wallstreet';
+        $userData->password = 'password123';
+
+        $user->set($userData);
+
+        $result = true;
+
+        foreach ($userData as $key => $value) {
+            if ($userData->$key !== $user->get()->$key) {
+                $result = false;
+            }
+            /*
+              // To debug a test, use echo to print on the terminal
+              echo "\r\n";
+              echo $u->$key;
+              echo "\r\n";
+              echo $user->get()->$key;
+             */
+        }
+
+        $this->assertTrue($result);
+    }
+
+}
