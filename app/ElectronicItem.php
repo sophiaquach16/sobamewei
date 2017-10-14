2e20bf8bd4217e7d96dc463ae76942f7a75bab71
Yufeng Ding <aimixiu1993@gmail.com>
2017-10-14 17:38:33 -0400

    Moddifications for ElectronicSpecification class
    Added new attribute and methodes for ElectrinicSpecifications for adding quantity when admin trying to update storage, and added methodes to generate model and series numbers for items.
    Added new class ElectronicItem, still need implementations.
    Modified database model according to modified Class.

---
+++ app/ElectronicItem.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 2017-10-13
+ * Time: 6:05 PM
+ */
+
+namespace App;
+
+
+class ElectronicItem
+{
+    private $serialNumber;
+
+    function __construct(){
+
+    }
+}
--- app/ElectronicSpecification.php
+++ app/ElectronicSpecification.php
@@ -2,7 +2,8 @@
 
 namespace App;
 
-class ElectronicSpecification {
+class ElectronicSpecification
+{
 
     private $id;
     private $dimension;
@@ -21,60 +22,95 @@ class ElectronicSpecification {
     private $ElectronicType_id;
     private $ElectronicType_name;
     private $ElectronicType_dimensionUnit;
+    private $items;
 
 
-  function __construct() {
+    function __construct()
+    {
         $argv = func_get_args();
-        switch( func_num_args() ) {
+        switch (func_num_args()) {
+            case 0:
+                self::__construct0();
+                break;
             case 1:
                 self::__construct1($argv[0]);
                 break;
-         }
+        }
     }
 
-    function __construct1($data) {
+    function __construct0()
+    {
+        $this->items = array();
+    }
+
+    function __construct1($data)
+    {
+        $this->items = array();
         $this->set($data);
     }
 
-    public function set($data) {
-      //mettre isset pour
-        if(isset($data->id))
-        $this->id = $data->id;
-        if(isset($data->dimension))
-        $this->dimension = $data->dimension;
-        if(isset($data->weight))
-        $this->weight = $data->weight;
-        if(isset($data->modelNumber))
-        $this->modelNumber = $data->modelNumber;
-        if(isset($data->brandName))
-        $this->brandName = $data->brandName;
-        if(isset($data->hdSize))
-        $this->hdSize = $data->hdSize;
-        if(isset($data->price))
-        $this->price = $data->price;
-        if(isset($data->processorType))
-        $this->processorType = $data->processorType;
-        if(isset($data->ramSize))
-        $this->ramSize = $data->ramSize;
-        if(isset($data->cpuCores))
-        $this->cpuCores = $data->cpuCores;
-        if(isset($data->batteryInfo))
-        $this->batteryInfo = $data->batteryInfo;
-        if(isset($data->os))
-        $this->os = $data->os;
-        if(isset($data->camera))
-        $this->camera = $data->camera;
-        if(isset($data->touchScreen))
-        $this->touchScreen = $data->touchScreen;
-        if(isset($data->ElectronicType_id))
-        $this->ElectronicType_id = $data->ElectronicType_id;
-        if(isset($data->ElectronicType_name))
-        $this->ElectronicType_name = $data->ElectronicType_name;
-        if(isset($data->ElectronicType_dimensionUnit))
-        $this->ElectronicType_dimensionUnit = $data->ElectronicType_dimensionUnit;
+    public function addItem()
+    {
+        if($this->quantity == 0){
+
+        }
     }
 
-    public function get() {
+    private function generateSerialNumber() {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $serialNumber = '';
+
+        for ($i = 0; $i < 12; $i++) {
+            $serialNumber .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        $serialNumber .= 1;
+
+        return $serialNumber;
+    }
+
+    public function set($data)
+    {
+        //mettre isset pour
+        if (isset($data->id))
+            $this->id = $data->id;
+        if (isset($data->dimension))
+            $this->dimension = $data->dimension;
+        if (isset($data->weight))
+            $this->weight = $data->weight;
+        if (isset($data->modelNumber))
+            $this->modelNumber = $data->modelNumber;
+        if (isset($data->brandName))
+            $this->brandName = $data->brandName;
+        if (isset($data->hdSize))
+            $this->hdSize = $data->hdSize;
+        if (isset($data->price))
+            $this->price = $data->price;
+        if (isset($data->processorType))
+            $this->processorType = $data->processorType;
+        if (isset($data->ramSize))
+            $this->ramSize = $data->ramSize;
+        if (isset($data->cpuCores))
+            $this->cpuCores = $data->cpuCores;
+        if (isset($data->batteryInfo))
+            $this->batteryInfo = $data->batteryInfo;
+        if (isset($data->os))
+            $this->os = $data->os;
+        if (isset($data->camera))
+            $this->camera = $data->camera;
+        if (isset($data->touchScreen))
+            $this->touchScreen = $data->touchScreen;
+        if (isset($data->ElectronicType_id))
+            $this->ElectronicType_id = $data->ElectronicType_id;
+        if (isset($data->ElectronicType_name))
+            $this->ElectronicType_name = $data->ElectronicType_name;
+        if (isset($data->ElectronicType_dimensionUnit))
+            $this->ElectronicType_dimensionUnit = $data->ElectronicType_dimensionUnit;
+    }
+
+    public function get()
+    {
         $returnData = new \stdClass();
 
         $returnData->id = $this->id;
@@ -98,8 +134,9 @@ class ElectronicSpecification {
         return $returnData;
     }
 
-    public function getModelNumber() {
-      return $this->modelNumber;
+    public function getModelNumber()
+    {
+        return $this->modelNumber;
     }
 
 }
