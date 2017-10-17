b0e2f6fb01b47daab65c4cf4b7feadc80f19e1ad
Melissa Duong <meliduong13@gmail.com>
2017-10-17 16:50:17 -0400

    fixed empty catalog values

--- tests/Unit/ElectronicCatalogTest.php
+++ tests/Unit/ElectronicCatalogTest.php
@@ -12,35 +12,49 @@ use App\Classes\Core\ElectronicSpecification;
 class ElectronicCatalogTest extends TestCase
 {
   public function testSetTest(){
-    $item1info=array("id"=>"abc123", "serialNumber"=>123, "ElectronicSpecification_id"=>"123abc");
-    $item2info=array("id"=>"abc456", "serialNumber"=>456, "ElectronicSpecification_id"=>"456abc");
-    $item1=new ElectronicItem($item1info);
-    $item2=new ElectronicItem($item2info);
-    $electronicItemsArray=array($item1,$item2);
-    $electroSpecData=array( "id"=>1,
-                            "dimensions"=>"1x2x3",
-                            "weight"=>1,
-                            "modelNumber"=>1,
-                            "modelNumber"=>1,
-                            "brandNumber"=>1,
-                            "hdSize"=>'1',
-                            "price"=>1,
-                            "processorType"=>'1',
-                            "ramSize"=>1,
-                            "cpuCores"=>1,
-                            "batteryInfo"=>'1',
-                            "os"=>'1',
-                            "camera"=>true,
-                            "touchScreen"=>true,
-                            "displaySize"=>'1',
-                            "ElectronicType_id"=>'1',
-                            "ElectronicType_name"=>'1',
-                            "ElectronicType_dimensionUnit"=>'1',
-                            "ElectronicType_displaySizeUnit"=>'1',
-                            "electronicItems"=>$electronicItemsArray,
-                            );
+
+    $electronicSpecification = new ElectronicSpecification();
+    $electronicItem1 = new ElectronicItem();
+    $electronicItem2 = new ElectronicItem();
+
+    $item1Data=new \stdClass();
+    $item1Data->id="abc123";
+    $item1Data->serialNumber=123;
+    $item1Data->ElectronicSpecification_id="123abc";
+
+    $item2Data=new \stdClass();
+    $item2Data->id="abc456";
+    $item2Data->serialNumber=456;
+    $item2Data->ElectronicSpecification_id="456abc";
+    $electronicItem1->set($item1Data);
+    $electronicItem2->set($item2Data);
+    $electronicItems=array($electronicItem1,$electronicItem2);
+
+    $electronicData= new \stdClass();
+
+    $electronicData->id = '1';
+    $electronicData->dimension = '2X3X4';
+    $electronicData->weight = '100';
+    $electronicData->modelNumber = '123';
+    $electronicData->brandName = '123';
+    $electronicData->hdSize = '123';
+    $electronicData->price = '123';
+    $electronicData->processorType = 'intel';
+    $electronicData->ramSize = '16';
+    $electronicData->cpuCores = '4';
+    $electronicData->batteryInfo = 'infinite';
+    $electronicData->os = 'ubuntu';
+    $electronicData->camera = true;
+    $electronicData->touchScreen = true;
+    $electronicData->ElectronicType_id = '1';
+    $electronicData->displaySize = '1';
+    $electronicData->electronicItems = $electronicItems;
+
+    $electronicSpecification->set($electronicData);
+
     $electronicCatalog = new ElectronicCatalog();
-    $electronicCatalog->setESList(array($electroSpecData));
-    $this->assertTrue(count($electronicCatalog->getESList())==1);
+    $electronicCatalog->setESList(array($electronicData));
+    $catalogList = $electronicCatalog->getESList();
+    $this->assertTrue(sizeof($catalogList)==sizeof(array($electronicData)));
+    }
   }
-}
