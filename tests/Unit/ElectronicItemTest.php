68b2be42709e7f71310082f1f484ce16f6f05313
Melissa Duong <meliduong13@gmail.com>
2017-10-21 01:23:06 -0400

    finished ElectronicItemTest

--- tests/Unit/ElectronicItemTest.php
+++ tests/Unit/ElectronicItemTest.php
@@ -9,16 +9,15 @@ use App\Classes\Core\ElectronicItem;
 
 class ElectroniItemTests extends TestCase{
 
-  public function testSetGet(){
+  public function testsetGet(){
     $itemData=new \stdClass();
     $itemData->id=1;
     $itemData->serialNumber=123;
-    $itemData->ElectronicSpecification_id="123";
+    $itemData->ElectronicSpecification_id=123;
 
     $electronicItem = new ElectronicItem();
     $electronicItem->set($itemData);
     $electronicItem->get();
-    var_dump($electronicItem->get());
     $electronicItemJson=(json_decode(json_encode($electronicItem->get()),true));
 
     //flag for value comparison. if items don't match, $valuesMatch becomes false
@@ -31,7 +30,39 @@ class ElectroniItemTests extends TestCase{
     else
       $valuesMatch=false;
     $this->assertTrue($valuesMatch);
-
   }//end of testSetGet
 
+  public function testgetId (){
+    $itemData=new \stdClass();
+    $itemData->id=1;
+    $itemData->serialNumber=123;
+    $itemData->ElectronicSpecification_id=123;
+    $electronicItem = new ElectronicItem();
+    $electronicItem->set($itemData);
+    $retrievedId=$electronicItem->getId();
+    $this->assertTrue($retrievedId==$itemData->id);
+  }
+
+  //test for both setSerialNumber and getSerialNumber
+  public function testsetGetSerialNumber (){
+    $itemData=new \stdClass();
+    $itemData->id=1;
+    $itemData->serialNumber=123;
+    $itemData->ElectronicSpecification_id=123;
+    $electronicItem = new ElectronicItem();
+    $electronicItem->setSerialNumber($itemData->serialNumber);
+    $retrievedSerialNumber=$electronicItem->getSerialNumber();
+    $this->assertTrue($retrievedSerialNumber==$itemData->serialNumber);
+  }
+
+  public function testgetElectronicSpecification_id(){
+    $itemData=new \stdClass();
+    $itemData->id=1;
+    $itemData->serialNumber=123;
+    $itemData->ElectronicSpecification_id=123;
+    $electronicItem = new ElectronicItem();
+    $electronicItem->set($itemData);
+    $retrievedElectronicSpecificationId=$electronicItem->getElectronicSpecification_id();
+    $this->assertTrue($retrievedElectronicSpecificationId==$itemData->ElectronicSpecification_id);
+  }
 }
