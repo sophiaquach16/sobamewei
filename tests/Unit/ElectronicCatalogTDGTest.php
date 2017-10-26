fb6ea26532654d1fd28a0b6385b5e9376fcb9c60
Helen2hang <he_hang@live.concordia.ca>
2017-10-26 16:36:12 -0400

    Testing method for updateElectronicSpecification of ElectronicCatalogTDG

--- tests/Unit/ElectronicCatalogTDGTest.php
+++ tests/Unit/ElectronicCatalogTDGTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Classes\TDG\ElectronicCatalogTDG;
 use App\Classes\Core\ElectronicSpecification;
+use App\Classes\Core\ElectronicItem;
 
 class ElectronicCatalogTDGTest extends TestCase {
 
@@ -52,4 +53,51 @@ class ElectronicCatalogTDGTest extends TestCase {
         $this->assertTrue($sameValues);
     }
 
+     public function testUpdateES(){
+        $electronicCatalogTDG = new ElectronicCatalogTDG();
+        $newES= new ElectronicSpecification();
+         
+        $newESData= new \stdClass();
+        $newESData->id = 1;
+        $newESData->dimension = '200 x 400 x 500';
+        $newESData->weight = 500;
+        $newESData->modelNumber = 'AHZ3011HLAZ';
+        $newESData->brandName = 'Samsung';
+        $newESData->hdSize = '600';
+        $newESData->price = '1200';
+        $newESData->processorType = 'AMD';
+        $newESData->ramSize = '8';
+        $newESData->cpuCores = '3';
+        $newESData->batteryInfo = '6 hours';
+        $newESData->os = 'Windows';
+        $newESData->camera = 1;
+        $newESData->touchScreen = 1;
+        $newESData->displaySize = '20 x 40';
+        $newESData->ElectronicType_id = 3;
+        
+        $newES->set($newESData);
+        
+        $electronicCatalogTDG->updateElectronicSpecification($newES);
+        
+        $this->assertDatabaseHas('ElectronicSpecification', [
+            'id' => 1,
+            'dimension' => '200 x 400 x 500',
+            'weight' => 500,
+            'modelNumber' => 'AHZ3011HLAZ',
+            'brandName' => 'Samsung',
+            'hdSize' => '600',
+            'price' => '1200',
+            'processorType' => 'AMD',
+            'ramSize' => '8',
+            'cpuCores' => '3',
+            'batteryInfo' => '6 hours',
+            'os' => 'Windows',
+            'camera' => 1,
+            'touchScreen' => 1,
+            'displaySize' => '20 x 40',
+            'ElectronicType_id' => 3,
+        ]);
+        
+     } 
+     
 }
