2192325bd18d2d60192ff308af7eba71039cbf98
Helen2hang <he_hang@live.concordia.ca>
2017-10-26 16:39:02 -0400

    Testing method for insertElectronicItem of ElectronicCatalogTDG

--- tests/Unit/ElectronicCatalogTDGTest.php
+++ tests/Unit/ElectronicCatalogTDGTest.php
@@ -100,4 +100,44 @@ class ElectronicCatalogTDGTest extends TestCase {
         
      } 
      
+     
+    public function testInsertEI() {
+        $electronicCatalogTDG = new ElectronicCatalogTDG();
+        $eS = new ElectronicSpecification(); 
+
+        $eSData = new \stdClass();
+        $eSData->id = 2;
+        $eSData->dimension = '100 x 200 x 300';
+        $eSData->weight = 400;
+        $eSData->modelNumber = 'ABC123DEF5D';
+        $eSData->brandName = 'LG';
+        $eSData->hdSize = '500';
+        $eSData->price = '1000';
+        $eSData->processorType = 'AMD';
+        $eSData->ramSize = '16';
+        $eSData->cpuCores = '4';
+        $eSData->batteryInfo = '12 hours';
+        $eSData->os = 'Windows';
+        $eSData->camera = 1;
+        $eSData->touchScreen = 1;
+        $eSData->displaySize = '10 x 30';
+        $eSData->ElectronicType_id = 3;
+
+        $eS->set($eSData);
+        
+        
+        $electronicCatalogTDG->insertElectronicSpecification($eS);
+        
+        $eIData = new \stdClass();        
+        $eIData->id = 1;
+        $eIData->serialNumber = "ABC123";
+        $eIData->ElectronicSpecification_id = 2;
+        
+        $electronicCatalogTDG->insertElectronicItem($eSData->modelNumber, $eIData);
+        
+        $this->assertDatabaseHas('ElectronicItem', [
+            'id' => 1,
+            'serialNumber' => 'ABC123',
+        ]);
+    }     
 }
