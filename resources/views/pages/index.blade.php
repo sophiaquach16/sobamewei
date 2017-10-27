ab07ea41e29b5bad8c07244cacf54e4beeb3f54a
z-alex <al_zhang@outlook.com>
2017-10-27 13:00:17 -0400

    Implemented criteria combination for the sorting and filtering

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,78 +4,86 @@
 
 
 <div>
-   <nav class="navbar navbar-inverse navbar-default" role="navigation">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-
     <nav class="navbar navbar-inverse navbar-default" role="navigation">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
             </button>
-          </div>
+        </div>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-      <div>
-            <ul class="nav navbar-nav">
-              <li><a href="/?1=desktop">Desktop Computers</a></li>
-              <li><a href="/?1=laptop">Laptops</a></li>
-              <li><a href="/?1=monitor">Monitors</a></li>
-              <li><a href="/?1=tablet">Tablets</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sort By <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="/?1=priceAscending">Price Ascending</a></li>
-                  <li><a href="/?1=priceDescending">Price Descending</a></li>
+        <nav class="navbar navbar-inverse navbar-default" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div>
+                <ul class="nav navbar-nav">
+                    <li><a href="/?eSType=desktop">Desktop Computers</a></li>
+                    <li><a href="/?eSType=laptop">Laptops</a></li>
+                    <li><a href="/?eSType=monitor">Monitors</a></li>
+                    <li><a href="/?eSType=tablet">Tablets</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sort By <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <?php
+                            $string = "";
+                            foreach ($lastInputs as $key => $value) {
+                                if ($key !== "sortBy") {
+                                    $string .= $key . "=" . $value . "&";
+                                }
+                            }
+                            ?>
+                            <li><a href="/?{{$string}}sortBy=priceAscending">Price Ascending</a></li>
+                            <li><a href="/?{{$string}}sortBy=priceDescending">Price Descending</a></li>
+                        </ul>
+                    </li>
                 </ul>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              </li>
-            </ul>
-      </div><!-- /.navbar-collapse -->
-    </div>
+                <ul class="nav navbar-nav navbar-right">
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+            </div>
 
-<div class="col-md-10">
+            <div class="col-md-10">
 
-        <div class="page-header">
-          <h2>Catalog</h2>
-        </div>
+                <div class="page-header">
+                    <h2>Catalog</h2>
+                </div>
 
-        @if (! empty($electronicSpecifications))
-        @foreach ($electronicSpecifications as $eS)
+                @if (! empty($electronicSpecifications))
+                @foreach ($electronicSpecifications as $eS)
 
-        <div class="col-md-2">
-            @if ( $eS->brandName )
-            {{$eS->brandName}}
-            @endif
-            @if ( $eS->ElectronicType_name )
-            {{$eS->ElectronicType_name}}
-            @endif
-            <br/>
-            @if ( $eS->modelNumber )
-            Model {{$eS->modelNumber}}
-            @endif
-            <br/>
-            @if ( $eS->price )
-            Price: ${{$eS->price}}
-            @endif
-        </div>
+                <div class="col-md-2">
+                    @if ( $eS->brandName )
+                    {{$eS->brandName}}
+                    @endif
+                    @if ( $eS->ElectronicType_name )
+                    {{$eS->ElectronicType_name}}
+                    @endif
+                    <br/>
+                    @if ( $eS->modelNumber )
+                    Model {{$eS->modelNumber}}
+                    @endif
+                    <br/>
+                    @if ( $eS->price )
+                    Price: ${{$eS->price}}
+                    @endif
+                </div>
 
-        @endforeach
-        @endif
+                @endforeach
+                @endif
 
-</div>
+            </div>
 
-@stop
+            @stop
