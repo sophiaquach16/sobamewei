ec7e378451b468c0a01977f0eafe2936d272e4aa
Batoul Yehia <batoul.yehia@gmail.com>
2017-10-26 23:35:02 -0400

    added a vertical navigation on the left

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -25,3 +25,61 @@
             </div>
         </div>
     </nav>
+    <div class="row affix-row">
+        <div class="col-sm-3 col-md-2 affix-sidebar">
+    		<div class="sidebar-nav">
+      <div class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <span class="visible-xs navbar-brand">Sidebar menu</span>
+        </div>
+        <div class="navbar-collapse collapse sidebar-navbar-collapse">
+          <ul class="nav navbar-nav" id="sidenav01">
+            <li class="active">
+              <a href="#" data-toggle="collapse" data-target="#toggleDemo0" data-parent="#sidenav01" class="collapsed">
+              <h4>
+              Control Panel
+              <br>
+              <small>IOSDSV <span class="caret"></span></small>
+              </h4>
+              </a>
+              <div class="collapse" id="toggleDemo0" style="height: 0px;">
+                <ul class="nav nav-list">
+                  <li><a href="#">ProfileSubMenu1</a></li>
+                  <li><a href="#">ProfileSubMenu2</a></li>
+                  <li><a href="#">ProfileSubMenu3</a></li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a href="#" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">
+              <span class="glyphicon glyphicon-cloud"></span> Submenu 1 <span class="caret pull-right"></span>
+              </a>
+              <div class="collapse" id="toggleDemo" style="height: 0px;">
+                <ul class="nav nav-list">
+                  <li><a href="#">Submenu1.1</a></li>
+                  <li><a href="#">Submenu1.2</a></li>
+                  <li><a href="#">Submenu1.3</a></li>
+                </ul>
+              </div>
+            </li>
+
+            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Desktop Computers</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Laptops</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Monitors</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Tablets</a></li>
+
+          </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    	</div>
+    	<div class="col-sm-9 col-md-10 affix-content">
+    		<div class="container">
+
+    				<div class="page-header">
