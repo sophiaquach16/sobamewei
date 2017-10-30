0b30f04efc1be7c897bbaa7c403b22216d047c76
sophiaquach16 <sophiaquach16@gmail.com>
2017-10-29 21:44:47 -0400

    Added Bootstrap Grid to ES

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -5,73 +5,132 @@
 
 @if($eS->ElectronicType_name === "Desktop")
 <h2>{{$eS->brandName}} {{$eS->ElectronicType_name}} - Model {{$eS->modelNumber}}</h2>
-<p>
-  Dimension: {{$eS->dimension}} cm<br>
-  Weight: {{$eS->weight}} kg<br>
-  RAM size: {{$eS->ramSize}} GB<br>
-  Number of CPU cores: {{$eS->cpuCores}}<br>
-  Hard Drive Size: {{$eS->hdSize}} GB<br>
-  Price: ${{$eS->price}}<br>
-</p>
-
+<div class="attributes">Dimension:</div>
+<div class="values">{{$eS->dimension}} cm</div>
+<br>
+<div class="attributes">Weight:</div>
+<div class="values">{{$eS->weight}} kg</div>
+<br>
+<div class="attributes">Number of CPU cores:</div>
+<div class="values">{{$eS->cpuCores}}</div>
+<br>
+<div class="attributes">Hard Drive Size:</div>
+<div class="values">{{$eS->hdSize}} GB</div>
+<br>
+<div class="attributes">Price:</div>
+<div class="values">${{$eS->price}}</div>
+<br>
 @endif
 
 @if($eS->ElectronicType_name === "Monitor")
 <h2>{{$eS->brandName}} {{$eS->ElectronicType_name}} - Model {{$eS->modelNumber}}</h2>
-<p>
-  Dimension: {{$eS->dimension}} in<br>
-  Weight: {{$eS->weight}} kg<br>
-  RAM size: {{$eS->ramSize}} GB<br>
-  Price: ${{$eS->price}}<br>
-</p>
+<div class="attributes">Dimension:</div>
+<div class="values">{{$eS->dimension}} cm</div>
+<br>
+<div class="attributes">Weight:</div>
+<div class="values">{{$eS->weight}} kg</div>
+<br>
+<div class="attributes">RAM size:</div>
+<div class="values">{{$eS->ramSize}} GB</div>
+<br>
+<div class="attributes">Price:</div>
+<div class="values">${{$eS->price}}</div>
+<br>
 @endif
 
 @if($eS->ElectronicType_name === "Laptop")
 <h2>{{$eS->brandName}} {{$eS->ElectronicType_name}} - Model {{$eS->modelNumber}}</h2>
-<p>
-  Dimension: {{$eS->dimension}} cm <br>
-  Display Size: {{$eS->displaySize}} in<br>
-  Weight: {{$eS->weight}} kg<br>
-  RAM size: {{$eS->ramSize}} GB<br>
-  Number of CPU cores: {{$eS->cpuCores}}<br>
-  Hard drive size: {{$eS->hdSize}}<br>
-  Battery information: {{$eS->batteryInfo}}<br>
-  Processor Type: {{$eS->processorType}} <br>
-  Built-in Operation System: {{$eS->os}} <br>
-  Camera:
+<div class="attributes">Dimension:</div>
+<div class="values">{{$eS->dimension}} cm</div>
+<br>
+<div class="attributes">Display Size:</div>
+<div class="values">{{$eS->displaySize}} cm</div>
+<br>
+<div class="attributes">Weight:</div>
+<div class="values">{{$eS->weight}} kg</div>
+<br>
+<div class="attributes">RAM size:</div>
+<div class="values">{{$eS->ramSize}} GB</div>
+<br>
+<div class="attributes">Number of CPU cores:</div>
+<div class="values">{{$eS->cpuCores}}</div>
+<br>
+<div class="attributes">Hard Drive Size:</div>
+<div class="values">{{$eS->hdSize}} GB</div>
+<br>
+<div class="attributes">Battery information:</div>
+<div class="values">{{$eS->batteryInfo}}</div>
+<br>
+<div class="attributes">Processor Type:</div>
+<div class="values">{{$eS->processorType}}</div>
+<br>
+<div class="attributes">Built-in Operation System:</div>
+<div class="values">{{$eS->os}}</div>
+<br>
+<div class="attributes">Camera:</div>
+<div class="values">
   @if(($eS->camera) === "1")
     yes
   @elseif(($eS->camera) === "0")
     no
-  @endif<br>
-  Touchscreen:
+  @endif
+</div>
+<br>
+<div class="attributes">Touchscreen:</div>
+<div class="values">
   @if(($eS->touchScreen) === "1")
-    yes
+    Yes
   @elseif(($eS->touchScreen) === "0")
-    no
-  @endif<br>
-  Price: ${{$eS->price}}<br>
-</p>
+    No
+  @endif
+</div>
+<br>
+<div class="attributes">Price:</div>
+<div class="values">${{$eS->price}}</div>
+<br>
 @endif
 
 @if($eS->ElectronicType_name === "Tablet")
 <h2>{{$eS->brandName}} {{$eS->ElectronicType_name}} - Model {{$eS->modelNumber}}</h2>
-Dimension: {{$eS->dimension}} cm<br>
-Display Size: {{$eS->displaySize}} in<br>
-Weight: {{$eS->weight}} kg<br>
-Processor: {{$eS->processorType}}<br>
-RAM Size: {{$eS->ramSize}} GB<br>
-CPU Cores: {{$eS->cpuCores}}<br>
-Hard drive size: {{$eS->hdSize}}<br>
-Battery Information: {{$eS->batteryInfo}}<br>
-Operating System: {{$eS->os}}<br>
-Camera:
-@if(($eS->camera) === "1")
-  yes
-@elseif(($eS->camera) === "0")
-  no
-@endif<br>
-Price: ${{$eS->price}}<br>
+<div class="attributes">Dimension:</div>
+<div class="values">{{$eS->dimension}} cm</div>
+<br>
+<div class="attributes">Display Size:</div>
+<div class="values">{{$eS->displaySize}} in</div>
+<br>
+<div class="attributes">Weight:</div>
+<div class="values">{{$eS->weight}} kg</div>
+<br>
+<div class="attributes">RAM size:</div>
+<div class="values">{{$eS->ramSize}} GB</div>
+<br>
+<div class="attributes">Number of CPU cores:</div>
+<div class="values">{{$eS->cpuCores}}</div>
+<br>
+<div class="attributes">Hard Drive Size:</div>
+<div class="values">{{$eS->hdSize}} GB</div>
+<br>
+<div class="attributes">Battery information:</div>
+<div class="values">{{$eS->batteryInfo}}</div>
+<br>
+<div class="attributes">Processor Type:</div>
+<div class="values">{{$eS->processorType}}</div>
+<br>
+<div class="attributes">Built-in Operation System:</div>
+<div class="values">{{$eS->os}}</div>
+<br>
+<div class="attributes">Camera:</div>
+<div class="values">
+  @if(($eS->camera) === "1")
+    yes
+  @elseif(($eS->camera) === "0")
+    no
+  @endif
+</div>
+<br>
+<div class="attributes">Price:</div>
+<div class="values">${{$eS->price}}</div>
+<br>
 @endif
 
 @endif
