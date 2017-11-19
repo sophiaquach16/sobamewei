29ee4b42716cd7ca6480878248a02f1e2c8d20cb
Batoul Yehia <batoul.yehia@gmail.com>
2017-11-19 11:21:15 -0500

    tested removeOutDatedEI

--- tests/Unit/ShoppingCartTest.php
+++ tests/Unit/ShoppingCartTest.php
@@ -126,6 +126,57 @@ class ShoppingCartTest extends TestCase
         $ShoppingCartList = $ShoppingCart->getEIList();
 
         $this->assertTrue($itemsList == $ShoppingCartList);
+    }
+
+    public function testRemoveOutdatedEI(){
+        //Creation of a user
+        $user = new User();
+
+        $userData = new \stdClass();
+
+        $userData->id = '1';
+        $userData->firstName = 'John';
+        $userData->lastName = 'Doe';
+        $userData->email = 'johndoe123@gmail.com';
+        $userData->phone = '123-456-7890';
+        $userData->admin = '0';
+        $userData->physicalAddress = '1234 Wallstreet';
+        $userData->password = 'password123';
+
+        $user->set($userData);
+        Auth::login($user);
+        Auth::check();
+
+        //Create outdated Electronic Item
+
+        $electronicItem1 = new ElectronicItem();
+        $electronicItem2 = new ElectronicItem();
+
+
+        $item1Data = new \stdClass();
+        $item1Data->id="1";
+        $item1Data->serialNumber = 123;
+        $item1Data->ElectronicSpecification_id = "1";
+        $item1Data->User_id='1';
+        $item1Data->expiryForUser='2017-10-10 12:12:12';
+        $electronicItem1->set($item1Data);
+
+        //Create a new electronic item, that isn't outdated
+        $item2Data = new \stdClass();
+        $item2Data->id="2";
+        $item2Data->serialNumber = 234;
+        $item2Data->ElectronicSpecification_id = "2";
+        $item2Data->User_id ='1';
+        $item2Data->expiryForUser='2017-12-25 12:12:12';
+        $electronicItem2->set($item2Data);
+
+        $electronicsList = array($electronicItem1,$electronicItem2);
+
+        $shoppingCart = ShoppingCart::getInstance();
+        $shoppingCart->setEIList($electronicsList);
+
+        //the list should be composed of one item only, since the first one is outdated
+        $this->assertTrue(count($shoppingCart->getEIList()) == 1);
 
 
 
