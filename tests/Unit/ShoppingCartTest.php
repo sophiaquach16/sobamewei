813aab1ffba31400a669c7e6a73e4a345686fa41
Batoul Yehia <batoul.yehia@gmail.com>
2017-11-19 12:27:30 -0500

    finished testing shoppingCart.php

--- tests/Unit/ShoppingCartTest.php
+++ tests/Unit/ShoppingCartTest.php
@@ -5,7 +5,6 @@ namespace Tests\Unit;
 use App\Classes\Core\ShoppingCart;
 use App\Classes\Core\ElectronicItem;
 use App\Classes\Core\User;
-use App\Classes\Core\ElectronicSpecification;
 use Tests\TestCase;
 use Hash;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +26,6 @@ class ShoppingCartTest extends TestCase
         $userData->admin = '0';
         $userData->physicalAddress = '1234 Wallstreet';
         $userData->password = 'password123';
-
 
         $user->set($userData);
 
@@ -68,12 +66,20 @@ class ShoppingCartTest extends TestCase
 
         $electronicItem3->set($item3Data);
 
-        $shoppingCart->addEIToCart($electronicItem3);
+        $shoppingCart->addEIToCart($electronicItem1);
         $shoppingCartList = $shoppingCart->getEIList();
         $size = count($shoppingCartList);
         $this->assertTrue($size == 1);
 
+        //try adding other items
+        $shoppingCart->addEIToCart($electronicItem2);
+        $newSize= count($shoppingCart->getEIList());
+        $this->assertTrue($newSize == 2);
 
+        //try adding other items
+        $shoppingCart->addEIToCart($electronicItem3);
+        $newerSize= count($shoppingCart->getEIList());
+        $this->assertTrue($newerSize == 3);
     }
 
 
@@ -177,13 +183,69 @@ class ShoppingCartTest extends TestCase
 
         //the list should be composed of one item only, since the first one is outdated
         $this->assertTrue(count($shoppingCart->getEIList()) == 1);
-
-
-
-
     }
 
+    public function testUpdateEIList(){
 
+        $user = new User();
+
+        $userData = new \stdClass();
+
+        $userData->id = "1";
+        $userData->firstName = 'John';
+        $userData->lastName = 'Doe';
+        $userData->email = 'johndoe123@gmail.com';
+        $userData->phone = '123-456-7890';
+        $userData->admin = '0';
+        $userData->physicalAddress = '1234 Wallstreet';
+        $userData->password = 'password123';
+
+        $user->set($userData);
+        Auth::login($user);
+        Auth::check();
+
+        //Create outdated Electronic Item
+
+        $electronicItem1 = new ElectronicItem();
+        $electronicItem2 = new ElectronicItem();
+
+
+        $item1Data = new \stdClass();
+        $item1Data->id="1";
+        $item1Data->serialNumber = 123;
+        $item1Data->ElectronicSpecification_id = "1";
+        $item1Data->User_id='1';
+        $item1Data->expiryForUser='2017-12-12 12:12:12';
+        $electronicItem1->set($item1Data);
+
+        //Create a new electronic item, that isn't outdated
+        $item2Data = new \stdClass();
+        $item2Data->id="2";
+        $item2Data->serialNumber = 234;
+        $item2Data->ElectronicSpecification_id = "2";
+        $item2Data->User_id ='1';
+        $item2Data->expiryForUser='2017-12-25 12:12:12';
+        $electronicItem2->set($item2Data);
+
+        $electronicsList = array($electronicItem1,$electronicItem2);
+
+        $shoppingCart = ShoppingCart::getInstance();
+        $shoppingCart->setEIList($electronicsList);
+
+        /*to remove an item from the shoppingCart, set UserID to null
+        and set the ExpiryForUser to null*/
+
+        $electronicItem1->setUserId(null);
+        $electronicItem1->setExpiryForUser(null);
+
+        $electronicsList = array($electronicItem1,$electronicItem2);
+        $shoppingCart->setEIList($electronicsList);
+        $itemsinCartAfterUpdate=count($shoppingCart->getEIList());
+
+        //we should expect only one item in the cart
+        $this->assertTrue($itemsinCartAfterUpdate == 1);
+
+    }
 
 
 
