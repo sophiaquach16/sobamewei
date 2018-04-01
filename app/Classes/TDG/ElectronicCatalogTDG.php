9d203b5281580d9563577db90f55fc47a333bf43
Marc <marc.leclair0113@gmail.com>
2018-04-01 06:25:41 -0400

    -added soft delete to electronicitem delete TDG method

--- app/Classes/TDG/ElectronicCatalogTDG.php
+++ app/Classes/TDG/ElectronicCatalogTDG.php
@@ -87,9 +87,12 @@ class ElectronicCatalogTDG {
         $queryString = substr($queryString, 0, -2);
         return $this->conn->query($queryString, $parameters);
     }
+
+    //set soft delete in OLD database (MySQL)
     public function deleteElectronicItem($electronicItem) {
-        $queryString = 'DELETE FROM ElectronicItem WHERE ';
-        $queryString .= 'id' . ' = :' . 'id';
+        $queryString = 'UPDATE ElectronicItem ';
+        $queryString .= 'SET last_forklift_or_change_check = -1 ';
+        $queryString .= ' WHERE id' . ' = :' . 'id';
 
         $parameters = new \stdClass();
         $parameters->id = $electronicItem->get()->id;
